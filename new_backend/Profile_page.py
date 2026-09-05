d3c436f29b2fc07160ceeebf6e55e79bc167ff7f
devjit2005 <devjitmanna0@gmail.com>
2026-09-05 23:15:50 +0530

    Add profile endpoint to retrieve user information

---
+++ new_backend/Profile_page.py
@@ -0,0 +1,12 @@
+@app.get("/profile")
+def profile(current_user=Depends(get_current_user)):
+
+    return {
+        "success": True,
+        "profile": {
+            "full_name": current_user.get("full_name"),
+            "email": current_user.get("email"),
+            "contact": current_user.get("contact"),
+            "location": current_user.get("location")
+        }
+    }
