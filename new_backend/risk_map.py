16427ff95ce15f894a14b1cf46cfc39daa3cfc57
devjit2005 <devjitmanna0@gmail.com>
2026-09-05 23:32:05 +0530

    Refactor risk_map to use FastAPI and MongoDB

--- new_backend/risk_map.py
+++ new_backend/risk_map.py
@@ -1,5 +1,29 @@
-@app.get("/risk-map")
-def risk_map(current_user=Depends(get_current_user)):
+from fastapi import APIRouter, Depends
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from dashboardfile import get_current_user
+import os
+
+
+load_dotenv()
+
+
+client = MongoClient(
+    os.getenv("mongodb_url")
+)
+
+database = client.get_database("user")
+
+risk_collection = database["risk_data"]
+
+
+router = APIRouter()
+
+
+@router.get("/risk-map")
+def risk_map(
+    current_user=Depends(get_current_user)
+):
 
     risks = list(
         risk_collection.find(
