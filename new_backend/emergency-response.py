d42bba634c7570dbfda953d680142e4c0acb6b7c
devjit2005 <devjitmanna0@gmail.com>
2026-09-05 23:32:40 +0530

    Refactor emergency response endpoint with MongoDB

--- new_backend/emergency-response.py
+++ new_backend/emergency-response.py
@@ -1,5 +1,31 @@
-@app.get("/emergency-response")
-def emergency_response(current_user=Depends(get_current_user)):
+from fastapi import APIRouter, Depends
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from dashboardfile import get_current_user
+import os
+
+
+load_dotenv()
+
+
+client = MongoClient(
+    os.getenv("mongodb_url")
+)
+
+database = client.get_database("user")
+
+emergency_collection = database[
+    "emergency_response"
+]
+
+
+router = APIRouter()
+
+
+@router.get("/emergency-response")
+def emergency_response(
+    current_user=Depends(get_current_user)
+):
 
     emergencies = list(
         emergency_collection.find(
